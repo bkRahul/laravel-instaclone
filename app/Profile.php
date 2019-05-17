755dda3a53d180b368d88f35b429f2f5f668408d
bkRahul <rahulsatya1068@gmail.com>
2019-05-17 23:31:19 +0530

    added profile and user model

--- app/Profile.php
+++ app/Profile.php
@@ -12,4 +12,16 @@ class Profile extends Model
     {
         return $this->belongsTo( User::class );
     }
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/oYZukJQHYAHWWdAceBR4nyI4R5f47huQhy6OvD98.png';
+        
+        return '/storage/'. $imagePath;
+    }
+    
+    public function followers()
+    {
+        return $this->belongsToMany( User::class );
+    }
 }
--- app/User.php
+++ app/User.php
@@ -37,6 +37,17 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($user){
+            $user->profile()->create([
+                'title' => $user->username
+            ]);
+        });
+    }
+
     public function profile()
     {
         return $this->hasOne( Profile::class );
@@ -46,4 +57,10 @@ class User extends Authenticatable
     {
         return $this->hasMany( Post::class )->orderBy('created_at', 'DESC');
     }
+
+    public function following()
+    {
+        return $this->belongsToMany( Profile::class );
+    }
+
 }
