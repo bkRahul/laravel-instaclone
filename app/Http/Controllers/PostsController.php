501cc1fda301afed5b9192641a41100e4462f81f
bkRahul <rahulsatya1068@gmail.com>
2019-05-19 09:40:10 +0530

    added index method to show all users posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,12 +6,24 @@ use Illuminate\Http\Request;
 
 use Intervention\Image\Facades\Image;
 
+use App\Post;
+
 class PostsController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth');
     }
+
+    public function index()
+    {
+        $users = auth()->user()->following()->pluck('profiles.user_id');
+
+        $posts = Post::whereIn('user_id', $users)->latest()->get();
+
+        return view('posts.index', compact('posts'));
+    }
+
     public function create()
     {
         return view('posts.create');
