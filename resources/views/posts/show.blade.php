4c03b4db3dbe6256c2ae50c862f4710466c0058f
bkRahul <rahulsatya1068@gmail.com>
2019-05-22 15:15:39 +0530

    added single post view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,8 +3,17 @@
 @section('content')
 <div class="container single_post">
     <div class="row">
-        <div class="col-md-6 single_post__img"><img src="/storage/{{ $post->image }}" alt=""></div>
-        <div class="col-md-6 single_post__details"><h4>{{ $post->user->username }}</h4><p>{{ $post->caption }}</p></div>
+        <div class="col-md-6 single_post__img">
+            <img src="/storage/{{$post->image}}" alt="{{ $post->caption }}">
+        </div>
+        <div class="col-md-6 single_post__details">
+            <div class="single_post__head">
+                <img src="/storage/{{$post->user->profile->profileImage()}}" alt="{{$post->user->profile->caption}}" class="rounded-circle">
+                <a href="/profile/{{$post->user->id}}"><h4 class="ml-3">{{ $post->user->username }}</h4></a>
+            </div>    
+            <hr />
+            <a href="/profile/{{$post->user->id}}"><p><b>{{$post->user->username}}</b></p></a><p>{{ $post->caption }}</p>
+        </div>
     </div>
 </div>
 @endsection
